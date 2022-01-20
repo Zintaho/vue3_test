5fbca2fdcac83bb7ceb01df4b63ad33b92c02e75
Zintaho <zintaho@gmail.com>
2022-01-20 17:57:48 +0900

    Sandbox2 Components

---
+++ src/Sandbox2.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1 ref="hmsg">{{headerMessage}}</h1>
+    <input v-model.lazy="headerMessage"/>
+    <MyBtn color="red">
+        <template #name>Apple</template>
+        <template #icon>(A)</template>
+    </MyBtn>
+    <MyBtn color="yellow">
+        <template #name>Banana</template>
+        <template #icon>(B)</template>
+    </MyBtn>
+    <MyBtn color="violet">
+        <template #name>Cherry</template>
+        <template #icon>(C)</template>
+    </MyBtn>
+    <my-btn
+        ref="lemon"
+        class="alter" 
+        :style="{color: 'royalblue'}"
+        @sing="waek"
+    >
+        <template #name>Lemon</template>
+        <template #icon>(L)</template>
+    </my-btn>
+</template>
+
+<script>
+import MyBtn from './components/MyBtn.vue';
+import {computed} from 'vue';
+export default {
+    components: {
+        MyBtn
+    },
+
+    data() {
+        return {
+            headerMessage : 'test'
+        }
+    },
+
+    mounted() {
+        console.log(this.$refs.lemon.$refs.btn);
+    },
+
+    methods: {
+        waek(event) {
+            console.log("waek", event.target);
+        }
+    },
+
+    provide() {
+        return {
+            hMsg : computed(() => this.headerMessage)
+        }
+    }
+}
+</script>
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,57 @@
+<template>
+<span>{{hMsg.value}}</span>
+  <span v-bind="$attrs">{</span>
+  <div
+    ref="btn"
+    :class="{
+        btn:true,
+        small
+    }"
+    :style="{
+        color
+    }"
+    @click="$emit('sing', $event)"
+  >
+      <slot name="icon" />
+      <slot name="name" />
+  </div>
+  <span v-bind="$attrs">}</span>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    emits: [
+        "sing"
+    ],
+    props: {
+        small: {
+            type: Boolean,
+            value: false
+        },
+        color: {
+            type: String,
+            value : 'gray'
+        }
+    },
+    inject: [
+        'hMsg'
+    ]
+}
+</script>
+
+<style scoped lang="scss">
+    .btn {
+        display: inline-block;
+        margin: 4px;
+        padding: 6px 12px;
+        border-radius: 4px;
+        color: white;
+        background-color: #222;
+        cursor: pointer;
+
+        &.small {
+            font-size: 0.7rem;
+        }
+    }
+</style>
