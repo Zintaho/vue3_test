08d938431074353a1cdb35467e376bcf69a4fd73
Zintaho <zintaho@gmail.com>
2022-01-21 10:17:19 +0900

    Sandbox3 Composition

---
+++ src/Sandbox3.vue
@@ -0,0 +1,58 @@
+<template>
+  <div @click="increase">
+    {{count}} / {{doubleCount}}
+  </div>
+  <div @click="changeMessage">
+    {{message}} / {{reversedMessage}}
+  </div>
+  <SB3Component
+    @gather="gather"
+    text = "감자"
+  />
+</template>
+
+<script>
+import {ref, computed, watch, onMounted} from 'vue';
+import SB3Component from './components/SB3Component.vue';
+export default {
+    components: {
+        SB3Component
+    },
+    setup() {
+        const count = ref(0);
+        const doubleCount = computed(() => count.value * 2);
+        function increase() {
+            count.value += 1;
+        }
+
+        const message = ref('Hello world!');
+        const reversedMessage = computed(() => message.value.split('').reverse().join(''));
+        function changeMessage() {
+            message.value += '!';
+        }
+        watch(message, (newValue) => {
+            console.log(newValue);
+        })
+
+        onMounted(() => {
+            console.log(count.value);
+        });
+
+        function gather() {
+            console.log("GATHER!");
+        }
+
+        return {
+            count, increase, doubleCount,
+            message, changeMessage, reversedMessage,
+            gather
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid black;
+    }
+</style>
---
+++ src/components/SB3Component.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="gather">
+      {{text}}
+  </button>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+export default {
+    props: {
+        text: {
+            type: String,
+            default: 'button'
+        }
+    },
+    emit: ['gather'],
+    setup(props, context) {
+        function gather() {
+            context.emit('gather');
+        }
+
+        onMounted(() => {
+            console.log(props.text);
+        })
+
+        return {
+            gather
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
