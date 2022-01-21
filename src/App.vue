d080ccb0f1c5978588c38391f134c237db7dc066
Zintaho <zintaho@gmail.com>
2022-01-21 13:18:55 +0900

    Movie page tutorial, header nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
+  <Header />
   <RouterView />
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-
+  components: {
+    Header
+  }
 }
 </script>
 
--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>About</h1>
+  <h1>Movie</h1>
 </template>
 
 <script>
